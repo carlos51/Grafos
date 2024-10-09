b0168a7a5482a465dd08a2ef6d0ca6a52ba9b6ff
Hertzz729 <101437898+Hertzz729@users.noreply.github.com>
2024-10-09 13:34:13 -0600

    Anuncios recompensados
    Se creo el script para inicializar los anuncios recompensados, y se añadió como atributo al botón de ver anuncios
    (El id es de Test)

--- Grafos/Assets/Scripts/MenuScrips/MainMenu.cs
+++ Grafos/Assets/Scripts/MenuScrips/MainMenu.cs
@@ -1,9 +1,13 @@
+using GoogleMobileAds.Api;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+
 public class MainMenu : MonoBehaviour
 {
+
     public void Play()
     {
         SceneManager.LoadSceneAsync(1);
